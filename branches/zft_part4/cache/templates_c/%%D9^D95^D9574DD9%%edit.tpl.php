40fb8e74d7beadfd6caab3159492169ed4a07e8f
kubek.bartosz <kubek.bartosz@a1b99a56-1c41-0410-8f1f-1d62bb77aca8>
2008-07-19 10:23:23 +0000

    Deleted unnecessary files

--- branches/zft_part4/cache/templates_c/%%D9^D95^D9574DD9%%edit.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.19, created on 2008-05-10 17:55:42
-         compiled from edit.tpl */ ?>
-    <h1><?php echo $this->_tpl_vars['title']; ?>
-</h1>
-    <?php $_smarty_tpl_vars = $this->_tpl_vars;
-$this->_smarty_include(array('smarty_include_tpl_file' => '_form.tpl', 'smarty_include_vars' => array()));
-$this->_tpl_vars = $_smarty_tpl_vars;
-unset($_smarty_tpl_vars);
- ?>
